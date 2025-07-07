ff289919b0846cc9e0639b8b83c9f389f04f9471
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2025-07-07 11:45:55 -0400

    dev

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.get('/generate', async (req, res) => {
     if (!fs.existsSync(filesDir)){
         fs.mkdirSync(filesDir);
     }
+    res.json({status:1, message: 'Welcome to ImagesGenerator!'});
 
     const website_url = req.query.url ? req.query.url : process.env.URL_DEV;
 
