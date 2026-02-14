feaf7be53f419388a10f5e288166e501df9f0602
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2026-02-14 00:23:30 -0400

    dev

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/generate', async (req, res) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.setViewport({
         width: 512,
